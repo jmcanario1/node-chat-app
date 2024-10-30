7d5ca6e8f628748957cef461998ba6de6b2aa4e7
João Marcelo <joao.canario@souunit.com.br>
2024-10-30 09:49:39 -0300

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-var { generateMessage } = require('./message')
+var { generateMessage, generateLocationMessage } = require('./message')
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,5 +11,18 @@ describe('generateMessage', () => {
 
         assert.strictEqual(typeof message.createdAt, 'number')
         assert.deepStrictEqual(message, { from, text, createdAt: message.createdAt })
+    })
+})
+
+describe('generateLocationMessage', () =>  {
+    it('should generate correct google maps link', () =>{
+        var from = 'Admin'
+        var latitude = -10.9674496
+        var longitude = -37.0540544
+        var url = `https://www.google.com/maps?q=${latitude},${longitude}`
+        var location = generateLocationMessage(from, latitude, longitude)
+
+        assert.strictEqual(typeof location.createdAt, 'number')
+        assert.deepStrictEqual(location, { from, url, createdAt: location.createdAt })
     })
 })
